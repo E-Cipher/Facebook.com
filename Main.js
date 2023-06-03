ef6b064dc075c24ed3aabe6705e5b631c6d87aca
Emmycipher <135451074+Emmycipher@users.noreply.github.com>
2023-06-03 17:53:12 +0100

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,8 @@
+document.getElementById("button1").addEventListener("click", function() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("Password").value;
+
+  // Log the email and password in the console
+  console.log("Email: " + email);
+  console.log("Password: " + password);
+});
